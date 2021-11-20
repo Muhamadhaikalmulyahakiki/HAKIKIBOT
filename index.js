89c0d8163226a3820e2d680bd13819b934f69bcd
Muhamadhaikalmulyahakiki <94756162+Muhamadhaikalmulyahakiki@users.noreply.github.com>
2021-11-21 01:58:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
 const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
+const instagram = 'https://instagram.com/muhamadhaikalmulyahakiki'; 
 const telegram = 'https://t.me/reuzzz'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://youtube.com/channel/UC3Os6LPNvccqOGTDnBim8dg';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
