94a34c98421dc43eb9feedd6481013b6956332c8
Muhamadhaikalmulyahakiki <94756162+Muhamadhaikalmulyahakiki@users.noreply.github.com>
2021-11-21 00:41:30 +0700

    Add files via upload

---
+++ Lib/donasi.js
@@ -0,0 +1,32 @@
+exports.donasi = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
+	return `🐼{ *MENU DONASI ${BotName}* }🐼
+  
+  
+TERIMA KASIH
+*${id.split("@s.whatsapp.net")[0]}*
+TELAH MEMBUKA MENU DONASI
+🐼🐼
+
+
+🗓*${tampilTanggal}* 🐼
+⏰*${tampilWaktu}* 🐼
+(Waktu Server)
+
+KALIAN BISA DONASI MENGGUNAKAN
+   
+🛡 *PULSA*: 085793443822
+🛡 *OVO*: 085793443822
+🛡 *DANA* : 085693502557
+🛡 *GOPAY* : 085793444833
+
+📺*IKLAN* *YOUTUBE HAKIKI TV*
+
+
+⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+FOLLOW INSTAGRAM SAYA
+${instagram}
+
+🐼TELEGRAM : ${telegram}
+
+`
+}
